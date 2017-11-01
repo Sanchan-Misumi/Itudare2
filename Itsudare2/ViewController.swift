7dfc257e0c5a9de5e33ce440d2205e94b45ada0c
Maho Misumi <maho.smile606@gmail.com>
2017-11-01 14:14:59 +0900

    doushitaIndex

--- Itsudare2/ViewController.swift
+++ Itsudare2/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet var dareLabel: UILabel!
     @IBOutlet var doushitaLabel: UILabel!
 
+    let ituArray: [String] = ["一年前", "昨日", "今日", "明日"]
+    let dokoArray: [String] = ["バイト先", "家", "電車", "学校"]
+    let dareArray: [String] = ["私", "チャコ", "母親", "ながたさん"]
+    let doushitaArray: [String] = ["怒った", "泣いた", "叫んだ", "寝た"]
+    
+    var Index: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -25,15 +32,45 @@ class ViewController: UIViewController {
     }
 
     @IBAction func change(){
+        itsuLabel.text = ituArray[Index]
+        dokoLabel.text = dokoArray[Index]
+        dareLabel.text = dareArray[Index]
+        doushitaLabel.text = doushitaArray[Index]
+        
+        Index = Index + 1
+        
+        if Index > 3{
+        Index = 0
+        }
         
     }
     
     @IBAction func reset(){
+        itsuLabel.text = "____"
+        dokoLabel.text = "____"
+        dareLabel.text = "____"
+        doushitaLabel.text = "____"
         
+        Index = 0
     }
     
     @IBAction func random(){
         
+        let itsuIndex = Int(arc4random_uniform(4))
+        let dokoIndex = Int(arc4random_uniform(4))
+        let dareIndex = Int(arc4random_uniform(4))
+        let doushitaIndex = Int(arc4random_uniform(4))
+        
+        print("いつ: \(itsuIndex)")
+        print("だれ: \(dareIndex)")
+        print("どこ: \(dokoIndex)")
+        print("どうした: \(doushitaIndex)")
+        
+        
+        itsuLabel.text = ituArray[itsuIndex]
+        dokoLabel.text = dokoArray[dokoIndex]
+        dareLabel.text = dareArray[dareIndex]
+        doushitaLabel.text = doushitaArray[doushitaIndex]
     }
 
 
